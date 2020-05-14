0b098224dc5ba180e698c4ac0155296d2fcb31e3
Roman Gayazov <gromver5@gmail.com>
2020-05-14 17:07:27 +0400

    refactor: rename components' exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export {
-  Provider as RjvProvider, ProviderRef as RjvProviderRef, ProviderContext as RjvProviderContext
+  Provider as ModelProvider, ProviderRef as ModelProviderRef, ProviderContext as ModelProviderContext
 } from './components/Provider'
-export { Subscribe as RjvSubscribe } from './components/Subscribe'
-export { Field as RjvField } from './components/Field'
-export { ModelProvided as RjvModelProvided } from './components/ModelProvided'
-export { RefsProvided as RjvRefsProvided } from './components/RefsProvided'
-export { Submit as RjvSubmit } from './components/Submit'
 export {
-  Scope as RjvScope, ScopeContext as RjvScopeContext
+  Scope as Scope, ScopeContext as ScopeContext
 } from './components/Scope'
+export { Subscribe as Subscribe } from './components/Subscribe'
+export { Field as Field } from './components/Field'
+export { ModelProvided as IfModelProvided } from './components/ModelProvided'
+export { RefsProvided as IfRefsProvided } from './components/RefsProvided'
+export { Submit as Submit } from './components/Submit'
